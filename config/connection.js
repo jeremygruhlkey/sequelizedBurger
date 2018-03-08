53e9496f7bd6bd4c05b3e3cb99f2f17d98a5c0ec
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-03-07 23:07:07 -0800

    Updated connection file for jawsDB and .env

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,29 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Lothario1",
-  database: "burger_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "Lothario1",
+//   database: "burger_db"
+// });
+
+require('dotenv').config()
+
+let connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+  })
+};
 
 // Make connection.
 connection.connect(function(err) {
